f2c359a1b6de348e81575fdaee5512f869c94273
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2023-12-10 17:46:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import FormValidator from './03-form-validator/FormValidator'
+import DarkMode from './02-dark-mode/DarkMode'
 
 function App () {
   return (
     <div className='App'>
       <FormValidator />
+      <DarkMode/>
     </div>
   )
 }
