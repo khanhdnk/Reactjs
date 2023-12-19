a247465b29cdc453e4060441fb425db33fc47789
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-12-19 17:49:59 +0700

    update

--- src/testRender/test.js
+++ src/testRender/test.js
@@ -1,28 +1,88 @@
 import { useState } from "react";
 
 export function TestRenderjs(){
+    const [data, setData] = useState('');
+    const [employee, setEmployee] = useState('');
+    const [name, setName] = useState('');
+    const [putId, setPutId] = useState('');
 
-    let number = 0;
-    // const [number, setNumber] = useState(0);
-    const setNumber = function(){
-        number++;
-        console.log(number); 
-        alert("clicked")
+    const getData = async() =>{
+        let finalData = '';
+        const response = await fetch(`http://localhost:3000/api/employees/`,{
+            headers: {
+                'x-api-key': "hello",
+                'Content-Type': 'application/json'
+            }
+        });
+        const arrayOfData = await response.json();
+        console.log(arrayOfData);
+        for(let i =0; i < arrayOfData.length; i++){
+            finalData+= <p>ID: {arrayOfData[i].id}, Name: {arrayOfData[i].name}</p>
+        }
+        finalData = arrayOfData.map(item => (
+            <p key={item.id}>ID: {item.id}, Name: {item.name}</p>
+          ));
+        console.log(arrayOfData[0].name)
+        return finalData;
     }
-    function logger({a,b,...rest}){
-        console.log(a);
-        console.log(b);
-        console.log(rest);
+
+    const getEmployee = async(id,name) =>{
+        let finalData;//try
+        const response = await fetch(`http://localhost:3000/api/employees/edit/${id}`,{
+            method: 'PUT',
+            headers: {
+                'x-api-key': "hello",
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name
+            })
+            
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const theData = await response.json();
+        if (theData.success === true){
+            return <p>success</p>;
+        }
+        else{
+            return null;
+        }
     }
-    let object = {a: 3,
-    b: 4,
-    c: 5
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
     }
-    logger(object);
+
+    
     return(
         <>
-            <h1>{number}</h1>
-            <button onClick={setNumber}>Click</button>
+            <div>
+                <h1>Get full data</h1>
+                {/* <p>{data? data.forEach(displayData): ''}</p> */}
+                {data}
+                <button onClick={async e => {setData(await getData(''));}}>Get method</button>
+
+            </div>
+            <hr/>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <h1>Put</h1>
+                    <label htmlFor="fid">Choose the ID:</label>
+                    <input value={putId} type="text" id="fid" name="fname" onChange={e=> setPutId(e.target.value)}/><br/>
+
+                    <label htmlFor="fname">Choose the Name:</label>
+                    <input value={name} type="text" id="fname" name="fname" onChange={e=> setName(e.target.value)}/><br/>
+                    <input type="submit" value="Submit" onClick={async e => setEmployee(await getEmployee(putId, name)) }/>
+                    {employee}
+                </form>
+            </div>
+            {/* <button onClick={}>Put method</button>
+            <button onClick={}>Post method</button>
+            <button onClick={}>Delete method</button> */}
         
         </>
     );
